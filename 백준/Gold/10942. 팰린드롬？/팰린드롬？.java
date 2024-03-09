cd22baaf3a1de869f376ee9a76c87161f55004ad
youngho <161284817+youngho98@users.noreply.github.com>
2024-03-09 20:16:46 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1492 ms, Memory: 215460 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        // 입력
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 질문
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()) - 1;
+            int e = Integer.parseInt(st.nextToken()) - 1;
+            if (check(s, e)) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static boolean check(int s, int e) {
+        while (s < e) {
+            if (arr[s] == arr[e]) {
+                s++;
+                e--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
