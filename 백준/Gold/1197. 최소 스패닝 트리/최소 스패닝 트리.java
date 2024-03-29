69c38e765b96c6af29662010014d9988d26c34ad
youngho <161284817+youngho98@users.noreply.github.com>
2024-03-29 15:20:37 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 648 ms, Memory: 50504 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,86 +1,81 @@
 import java.io.BufferedReader;
+import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int[] p;
+	
+	static class Edge implements Comparable<Edge>{
+		int x, y, w;
+		
+		public Edge(int x, int y, int w) {
+			this.x = x;
+			this.y = y;
+			this.w = w;
+		}
 
-    static int[] parents;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-
-        // 간선 입력
-        Line[] lines = new Line[e];
-        for (int i = 0; i < e; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1;
-            int b = Integer.parseInt(st.nextToken()) - 1;
-            int c = Integer.parseInt(st.nextToken());
-            lines[i] = new Line(a, b, c);
-        }
-
-        // 짧은 간선부터 정렬
-        Arrays.sort(lines, (o1, o2) -> {
-            return o1.w - o2.w;
-        });
-
-        // 유니온 파인드
-        parents = new int[v];
-        for (int i = 0; i < v; i++) {
-            parents[i] = i;
-        }
-
-        long weight = 0L;
-        int cnt = 0;
-        for (int i = 0; i < e; i++) {
-            if (unionFind(lines[i].p1, lines[i].p2)) {
-                continue;
-            }
-            union(lines[i].p1, lines[i].p2);
-            weight += lines[i].w;
-            if (cnt == v - 1) {
-                break;
-            }
-        }
-
-        System.out.println(weight);
-    }
-
-    static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x < y) {
-            parents[y] = x;
-        } else if (x > y) {
-            parents[x] = y;
-        }
-    }
-
-    static int find(int x) {
-        if (parents[x] == x) {
-            return x;
-        }
-        return find(parents[x]);
-    }
-
-    static boolean unionFind(int x, int y) {
-        if (find(x) == find(y)) {
-            return true;
-        }
-        return false;
-    }
-}
-
-class Line {
-    int p1, p2, w;
-
-    public Line(int p1, int p2, int w) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.w = w;
-    }
+		@Override
+		public int compareTo(Edge o) {
+			return this.w -o.w;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int v = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		
+		p = new int[v + 1];
+		
+		for (int i = 0; i < v + 1; i++) {
+			p[i] = i;
+		}
+		
+		Edge[] edges = new Edge[e];
+		
+		for (int i = 0; i < e; i++) {
+			st = new StringTokenizer(br.readLine());
+			edges[i] = new Edge(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+		}
+		
+		Arrays.sort(edges);
+		
+		int result = 0;
+		int pick = 0;
+		
+		for (int i = 0; i < e; i++) {
+			int px = findSet(edges[i].x);
+			int py = findSet(edges[i].y);
+		
+			if (px != py) {
+				union(px, py);
+				result += edges[i].w;
+				pick++;
+				
+			}
+			
+			if (pick == v - 1) break;
+		}
+		
+		System.out.println(result);
+	}
+	
+	
+	
+	public static int findSet(int x) {
+		if (p[x] != x) {
+			return p[x] = findSet(p[x]);
+		}
+		return p[x];
+	}
+	
+	public static void union(int x, int y) {
+		p[y] = x;
+	}
 }
