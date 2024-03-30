e73b2abb9e9263ed5d9b06e7c2d6f5e2f75a5b17
youngho <161284817+youngho98@users.noreply.github.com>
2024-03-30 23:45:53 +0900

    [Bronze IV] Title: 방학 숙제, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int l = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        System.out.println(l - Math.max((a - 1) / c + 1, (b - 1) / d + 1));
+    }
+}
