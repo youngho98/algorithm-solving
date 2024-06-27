eeec230141d0942ae0b049353c2bd82f70804809
youngho <161284817+youngho98@users.noreply.github.com>
2024-06-27 16:47:36 +0900

    [Bronze II] Title: 교수님 그림이 깨지는데요?, Time: 140 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/20205. 교수님 그림이 깨지는데요？/교수님 그림이 깨지는데요？.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n * k; i++) {
+            for (int j = 0; j < n * k; j++) {
+                sb.append(arr[i / k][j / k]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
