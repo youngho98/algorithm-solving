68edfc783737c7a0d0a9bc242df6b17053248757
youngho <161284817+youngho98@users.noreply.github.com>
2024-07-10 17:36:24 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,11 @@
+s = input()
+f = ""
+if s == "BHA":
+    f = "Branksome Hall Asia"
+elif s == "KIS":
+    f = "Korea International School"
+elif s == "SJA":
+    f = "St. Johnsbury Academy"
+else:
+    f = "North London Collegiate School"
+print(f)
