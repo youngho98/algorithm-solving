1fcff700e582412bfbd5597151d899b1cb8c2458
youngho <161284817+youngho98@users.noreply.github.com>
2024-12-27 21:22:26 +0900

    [Silver III] Title: 수열, Time: 24 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n;
+    cin >> k;
+
+    int psum[n + 1];
+    psum[0] = 0;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        psum[i + 1] = psum[i] + num;
+    }
+
+    int max = -100000000;
+    for (int i = 0; i + k <= n; i++) {
+        if (max < psum[i + k] - psum[i]) {
+            max = psum[i + k] - psum[i];
+        }
+    }
+
+    cout << max;
+    
+    return 0;
+}
