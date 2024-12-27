807c434b7bfd7b078806acd4c98de4afa7585e37
youngho <161284817+youngho98@users.noreply.github.com>
2024-12-27 21:46:40 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 240 ms, Memory: 23548 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    map<string, int> alphaDict;
+    string numDict[n + 1];
+
+    for (int i = 1; i <= n; i++) {
+        string name;
+        cin >> name;
+        alphaDict.insert({name, i});
+        numDict[i] = name;
+    }
+
+    string ans;
+    for (int i = 0; i < m; i++) {
+        string q;
+        cin >> q;
+        if ('A' <= q[0] && q[0] <= 'Z') {
+            ans += to_string(alphaDict[q]);
+            ans += "\n";
+        } else {
+            ans += numDict[stoi(q)];
+            ans += "\n";
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
