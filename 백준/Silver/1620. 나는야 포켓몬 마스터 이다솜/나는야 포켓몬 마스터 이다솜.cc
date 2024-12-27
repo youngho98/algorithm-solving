7732f34ab3c9b6e2eab252a8049c239835ee90fe
youngho <161284817+youngho98@users.noreply.github.com>
2024-12-27 21:47:43 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 152 ms, Memory: 23660 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -5,6 +5,9 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
     int n, m;
     cin >> n >> m;
     map<string, int> alphaDict;
