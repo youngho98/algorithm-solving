e4aa73ee93dfc5cbd50f539b6b9837063e256bb0
youngho <161284817+youngho98@users.noreply.github.com>
2025-07-26 19:04:54 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    int n, k;
+    cin >> n;
+    
+    priority_queue<int> pq;
+    for (int i = 0; i < n; i++) {
+        cin >> k;
+        if (k == 0) {
+            if (pq.empty()) {
+                cout << 0 << "\n";
+            } else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else {
+            pq.push(k);
+        }
+    }
+
+    return 0;
+}
