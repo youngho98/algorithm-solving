6f75afad30144105bd60e362999a7d02223e7617
youngho <161284817+youngho98@users.noreply.github.com>
2025-07-29 09:26:21 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <deque>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	deque<int> dq;
+	string cmd;
+	for (int i = 0; i < n; i++) {
+		cin >> cmd;
+		if (cmd == "push") {
+			int num;
+			cin >> num;
+			dq.push_back(num);
+		}
+		else if (cmd == "pop") {
+			if (!dq.empty()) {
+				cout << dq.front() << "\n";
+				dq.pop_front();
+			}
+			else {
+				cout << -1 << "\n";
+			}
+		}
+		else if (cmd == "size") {
+			cout << dq.size() << "\n";
+		}
+		else if (cmd == "empty") {
+			if (dq.empty()) {
+				cout << 1 << "\n";
+			}
+			else {
+				cout << 0 << "\n";
+			}
+		}
+		else if (cmd == "front") {
+			if (!dq.empty()) {
+				cout << dq.front() << "\n";
+			}
+			else {
+				cout << -1 << "\n";
+			}
+		}
+		else if (cmd == "back") {
+			if (!dq.empty()) {
+				cout << dq.back() << "\n";
+			}
+			else {
+				cout << -1 << "\n";
+			}
+		}
+	}
+
+	return 0;
+}
