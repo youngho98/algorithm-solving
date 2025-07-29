ba61362682d959b220da49fa3714504861c65dbf
youngho <161284817+youngho98@users.noreply.github.com>
2025-07-29 16:39:55 +0900

    [Gold III] Title: 수학은 너무 쉬워, Time: 32 ms, Memory: 10728 KB -BaekjoonHub

--- 백준/Gold/2904. 수학은 너무 쉬워/수학은 너무 쉬워.cc
+++ 백준/Gold/2904. 수학은 너무 쉬워/수학은 너무 쉬워.cc
@@ -11,6 +11,7 @@ int power(int base, int exp);
 
 int prime[1000001];
 int gcd_prime[1000001];
+vector<int> primes;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -38,7 +39,7 @@ int main() {
 
 	// 최대공약수 찾기
 	int gcd = 1;
-	for (int i = 2; i <= 1000000; i++) {
+	for (int i : primes) {
 		if (prime[i] >= n) {
 			gcd *= power(i, (prime[i] / n));
 		}
@@ -59,21 +60,23 @@ int main() {
 }
 
 void prime_find() {
-	prime[0] = prime[1] = -1;
 	for (int i = 2; i <= 1000; i++) {
-		if (prime[i] >= 0) {
-			int temp = 2 * i;
-			while (temp <= 1000000) {
-				prime[temp] = -1;
-				temp += i;
+		if (prime[i] == 0) {
+			for (int j = 2 * i; j <= 1000000; j += i) {
+				prime[j] = -1;
 			}
+		}
+	}
+	for (int i = 2; i <= 1000000; i++) {
+		if (prime[i] == 0) {
+			primes.push_back(i);
 		}
 	}
 }
 
 void prime_fac(int num) {
-	for (int i = 2; i <= 1000000; i++) {
-		while (num > 1 && prime[i] >= 0 && num % i == 0) {
+	for (int i : primes) {
+		while (num > 1 && num % i == 0) {
 			prime[i]++;
 			num /= i;
 		}
@@ -81,8 +84,8 @@ void prime_fac(int num) {
 }
 
 void find_gcd_prime(int num) {
-	for (int i = 2; i <= 1000000; i++) {
-		while (num > 1 && prime[i] >= 0 && num % i == 0) {
+	for (int i : primes) {
+		while (num > 1 && num % i == 0) {
 			gcd_prime[i]++;
 			num /= i;
 		}
@@ -91,7 +94,7 @@ void find_gcd_prime(int num) {
 
 int find_cnt(int num) {
 	int cnt = 0;
-	for (int i = 2; i <= 1000000; i++) {
+	for (int i : primes) {
 		for (int j = 0; j < gcd_prime[i]; j++) {
 			if (num % i == 0) {
 				num /= i;
