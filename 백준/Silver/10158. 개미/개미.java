73ca48293259289751e73e556386d1f265ceb633
youngho <161284817+youngho98@users.noreply.github.com>
2026-02-19 23:02:46 +0900

    [Silver III] Title: 개미, Time: 100 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -5,28 +5,24 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        // 입력
         StringTokenizer st = new StringTokenizer(br.readLine());
         int w = Integer.parseInt(st.nextToken());
         int h = Integer.parseInt(st.nextToken());
-
         st = new StringTokenizer(br.readLine());
         int p = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
-
         int t = Integer.parseInt(br.readLine());
 
-        int x = (p + t) % (2 * w);
-        int y = (q + t) % (2 * h);
-        if (x > w) {
-            x = (2 * w) - x;
-        }
-        if (y > h) {
-            y = (2 * h) - y;
-        }
-
+        // 계산
+        int x = (p + t) % (w * 2);
+        int y = (q + t) % (h * 2);
+        x = x > w ? w * 2 - x : x;
+        y = y > h ? h * 2 - y : y;
         sb.append(x).append(" ").append(y);
         System.out.println(sb);
     }
