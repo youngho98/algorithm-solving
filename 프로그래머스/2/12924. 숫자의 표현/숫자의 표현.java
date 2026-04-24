42930fee5d9de55cc8a99f6acb0bbf51b6ce2d61
youngho <161284817+youngho98@users.noreply.github.com>
2026-04-24 16:02:07 +0900

    Refactor solution method for number representation

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,12 +1,13 @@
+// 홀수개의 합일 때는 가운데 값이 자연수여야 하고,
+// 짝수개의 합일 때는 가운데 값이 x.5 여야 한다.
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = 1; i < n; i++) {
-            if (i * (i + 1) / 2 > n) break;
-            if ((i & 1) == 1 && n % 1 == 0) answer++;
-            else if ((i & 1) == 0 && n % 1 == 0.5) answer++;
+        for (int i = 1; i * (i + 1) <= n * 2; i++) {
+            if ((i & 1) == 1 && n % i == 0) answer++;
+            else if ((i & 1) == 0 && (2 * n) % i == 0 && (2 * n / i) % 2 == 1) answer++;
         }
-        System.out.println(3.5 % 1);
         return answer;
     }
 }
