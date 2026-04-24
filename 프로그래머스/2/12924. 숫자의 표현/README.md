f23bb8b14598914c543f8dc8e1d89a07f4acea98
youngho <161284817+youngho98@users.noreply.github.com>
2026-04-24 15:54:51 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=java) 
 
 ### 성능 요약
 
-메모리: 53.8 MB, 시간: 0.04 ms
+메모리: 78.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 24일 15:53:56
+2026년 04월 24일 15:54:49
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,17 +1,12 @@
-// i * (s + l) / 2 == n;
-// i * (s + l) = 2 * n;
-// i가 2n의 약수여야 한다.
-
-// 어떤 숫자 a 부터 i개의 숫자를 더한 합은?
-// (2 * a + i - 1) * i / 2 = n
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = 1; i * (i + 1) / 2 <= n; i++) {
-            if ((i & 1) == 1 && n % i == 0) answer++;
-            else if ((i & 1) == 0 && (2 * n) % i == 0 && (2 * n / i) % 2 == 1) answer++;
+        for (int i = 1; i < n; i++) {
+            if (i * (i + 1) / 2 > n) break;
+            if ((i & 1) == 1 && n % 1 == 0) answer++;
+            else if ((i & 1) == 0 && n % 1 == 0.5) answer++;
         }
+        System.out.println(3.5 % 1);
         return answer;
     }
 }
